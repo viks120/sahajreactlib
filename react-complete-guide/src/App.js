c2c21c874780b3ee59a399e789305c6f05b3a69a
Vikrant <vikrant@bitcoinira.com>
2019-02-15 16:42:31 -0800

    3:34 Lecture

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,6 +8,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am react app</h1>
         <Person />
+        <Person />
+        <Person />
       </div>
     );
   //  return React.createElement('div', {className: 'App'}, React.createElement('h1' , null, 'Hi, I\'m a App'));
