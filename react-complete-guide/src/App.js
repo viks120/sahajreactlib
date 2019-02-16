6cb1d6597946e4b8bed0692e295d1b7c61d346eb
Vikrant <vikrant@bitcoinira.com>
2019-02-15 16:37:44 -0800

    1st Function Component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-   // return (
-   //   <div className="App">
-   //     <h1>Hi, I am react app</h1>
-   //   </div>
-   // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1' , null, 'Hi, I\'m a App'));
+    return (
+      <div className="App">
+        <h1>Hi, I am react app</h1>
+        <Person />
+      </div>
+    );
+  //  return React.createElement('div', {className: 'App'}, React.createElement('h1' , null, 'Hi, I\'m a App'));
   }
 }
 
