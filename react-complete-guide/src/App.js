4bd7ed2734fafe98e14757e493c284f9bdea3103
Vikrant <viks120@gmail.com>
2019-02-17 20:56:44 -0800

    Set state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,14 +12,26 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () => {
+    //console.log('was clicked');
+    // Don't Do This this.state.persons[0].name = 'Vikrant';
+    this.setState({
+      persons: [
+        {name: "Vikrant", age: 34},
+        {name: "Monu", age: 30},
+        {name: "Mia", age: 0.5}
+      ]
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I am react app</h1>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].name}>Hobbies: Walking</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].name}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   //  return React.createElement('div', {className: 'App'}, React.createElement('h1' , null, 'Hi, I\'m a App'));
